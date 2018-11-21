e63637af2b8662617ff6176c6c95e856c0e50564
Nikkie Mashian <nikkiemashian@gmail.com>
2018-11-21 11:29:24 -0800

    deleted old db

--- routes/notes.js
+++ routes/notes.js
@@ -5,11 +5,6 @@ const express = require('express');
 // Create an router instance (aka "mini-app")
 const router = express.Router();
 
-// TEMP: Simple In-Memory Database
-// const data = require('../db/notes');
-// const simDB = require('../db/simDB');
-// const notes = simDB.initialize(data);
-
 //Gonna use our new database now!
 const knex = require('../knex');
 // loads the knex.js file which loads the knexfile.js and returns a connection to the database which is assigned to knex.
